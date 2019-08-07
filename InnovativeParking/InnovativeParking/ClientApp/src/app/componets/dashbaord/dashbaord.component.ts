ca343614b2ae89dd75be17c2fca7e54c8e26a453
shreyaskushal <shreyaskushal@gmail.com>
2019-08-07 09:44:57 +0000

    Dashboard Changes

--- InnovativeParking/InnovativeParking/ClientApp/src/app/componets/dashbaord/dashbaord.component.ts
+++ InnovativeParking/InnovativeParking/ClientApp/src/app/componets/dashbaord/dashbaord.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashbaordComponent implements OnInit {
 
-  constructor() { }
+  currentDate: number;
+  currentTime: any;
+
+  constructor() {
+
+    this.currentDate = Date.now();
+    this.currentTime = new Date().getHours() + ':' + new Date().getMinutes();
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
